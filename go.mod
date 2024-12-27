c99f690db5087cb35c33b667678d1d55d1f249f0
Abbas <muhdabbas98@gmail.com>
2024-12-27 11:34:44 +0800

    :sparkles: Add form

--- go.mod
+++ go.mod
@@ -2,8 +2,14 @@ module reading-list
 
 go 1.20
 
+require github.com/gorilla/mux v1.8.1
+
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240902231107-85af5b9d094d // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 )
--- main.go
+++ main.go
@@ -5,20 +5,24 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+
+	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 var tmpl *template.Template
 var db *sql.DB
 
-type reading struct {
-	id          int64
-	url         string
-	title       string
-	description string
-	added_date  string
-	added_time  string
+type Reading struct {
+	Id          int64
+	Url         string
+	Title       string
+	Description string
+	AddDate     string
+	AddTime     string
 }
 
 func init() {
@@ -27,7 +31,10 @@ func init() {
 
 func initDb() {
 	var err error
-	db, err = sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/reading_list")
+	database := os.Getenv("DATABASE")
+	token := os.Getenv("TOKEN")
+	url := "libsql://" + database + "?authToken=" + token
+	db, err = sql.Open("libsql", url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,15 +42,102 @@ func initDb() {
 	if err = db.Ping(); err != nil {
 		log.Fatal(err)
 	}
+
+	// Create readings table if it doesn't exist
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS readings (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		url TEXT NOT NULL,
+		title TEXT NOT NULL,
+		description TEXT,
+		add_date DATE DEFAULT CURRENT_DATE,
+		add_time TIME DEFAULT CURRENT_TIME
+	)`
+
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
+	initDb()
+	defer db.Close()
+
 	gRouter := mux.NewRouter()
+	gRouter.HandleFunc("/", Homepage)
+	gRouter.HandleFunc("/readings/{id}", ReadingDetails)
+	gRouter.HandleFunc("/newReadingForm", addReadingForm)
+	gRouter.HandleFunc("/readings/{id}/delete", DeleteReading)
+	gRouter.HandleFunc("/addReading", AddReading).Methods("POST")
+	gRouter.HandleFunc("/readings", fetchReadings)
+	err := http.ListenAndServe(":8080", gRouter)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
 
-	gRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.ExecuteTemplate(w, "index.html", nil)
-	})
+func Homepage(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "index.html", nil)
+}
 
-	http.ListenAndServe(":8080", gRouter)
+func fetchReadings(w http.ResponseWriter, r *http.Request) {
+	readings, _ := getReadings(db)
+	tmpl.ExecuteTemplate(w, "readingList", readings)
+}
 
+func ReadingDetails(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "readingDetails.html", nil)
+}
+
+func getReadings(db *sql.DB) ([]Reading, error) {
+	query := "SELECT * FROM readings"
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var readings []Reading
+
+	for rows.Next() {
+		var reading Reading
+		rowErr := rows.Scan(&reading.Id, &reading.Url, &reading.Title, &reading.Description, &reading.AddDate, &reading.AddTime)
+
+		if rowErr != nil {
+			return nil, rowErr
+		}
+		readings = append(readings, reading)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return readings, nil
+}
+
+func DeleteReading(w http.ResponseWriter, r *http.Request) {}
+
+func AddReading(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("url")
+	title := r.FormValue("title")
+	description := r.FormValue("description")
+
+	query := "INSERT INTO readings (url, title, description) VALUES (?, ?, ?)"
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	_, executeErr := stmt.Exec(url, title, description)
+	if executeErr != nil {
+		log.Fatal(executeErr)
+	}
+	readings, _ := getReadings(db)
+	tmpl.ExecuteTemplate(w, "readingList", readings)
+}
+
+func addReadingForm(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "addReadingForm", nil)
 }
